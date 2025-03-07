63c84b15985f594f56cfb603372f7ff96997ea81
Sanderley <sandernascimento98@gmail.com>
2025-03-06 21:20:35 -0300

    Mudei dubug para True

--- lojaproject/settings.py
+++ lojaproject/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)vd53lkglbi)42+eux^eu2nin1j+wh0$^_)e63$!g^9tm*l@@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
