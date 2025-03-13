6bab3cd6753eaf7c3213c1ce84cb6440ffaa3a9d
Sanderley <sandernascimento98@gmail.com>
2025-03-13 13:04:38 -0300

    Carrinho de compra quase completo

--- loja_app/views.py
+++ loja_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
 from .models import Produto, Pedido, Categoria
+from carrinho_app.models import Produto, Carrinho, ItemCarrinho
 from .forms import PagamentoForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -24,31 +25,37 @@ class ProdutosView(TemplateView):
         return context
 
 
-class PagamentoView(LoginRequiredMixin,TemplateView):
+class PagamentoView(LoginRequiredMixin, TemplateView):
     template_name = 'pagamentos/pagamento.html'
-    login_url='/usuarios/login/'
-    redirect_field_name='redirect_to'
+    login_url = '/usuarios/login/'
+    redirect_field_name = 'redirect_to'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        produto_id = self.kwargs.get('produto_id')
-        produto = get_object_or_404(Produto, id=produto_id)
+        carrinho = get_object_or_404(Carrinho, user=self.request.user)
+        itens_carrinho = ItemCarrinho.objects.filter(carrinho=carrinho)
         form = PagamentoForm()
-        context['produto'] = produto
+        total_preco = sum(item.total_preco for item in itens_carrinho)
+        context['itens_carrinho'] = itens_carrinho
         context['form'] = form
+        context['total_preco'] = total_preco
         return context
 
     def post(self, request, *args, **kwargs):
-        produto_id = self.kwargs.get('produto_id')
-        produto = get_object_or_404(Produto, id=produto_id)
+        carrinho = get_object_or_404(Carrinho, user=self.request.user)
+        itens_carrinho = ItemCarrinho.objects.filter(carrinho=carrinho)
         form = PagamentoForm(request.POST)
         if form.is_valid():
-            pedido = form.save(commit=False)
-            pedido.produto = produto
-            pedido.save()
-            # Adicione a lógica para processar o pagamento (Pix ou Cartão) aqui
+            for item in itens_carrinho:
+                pedido = form.save(commit=False)
+                pedido.produto = item.produto
+                pedido.preco_final = item.total_preco
+                pedido.save()
+                # Adicione a lógica para processar o pagamento (Pix ou Cartão) aqui
+            carrinho.delete()  # Limpar o carrinho após o pagamento
             return redirect('loja_app:sucesso_pagamento')
-        return self.render_to_response(self.get_context_data(form=form, produto=produto))
+        return self.render_to_response(self.get_context_data(form=form))
+
 
 class SucessoPagamentoView(TemplateView):
     template_name = 'pagamentos/sucesso_pagamento.html'
