ce6952fb2572ff2e45c5a9ea8b035ba6bed18d8f
Sanderley <sandernascimento98@gmail.com>
2025-04-10 22:19:36 -0300

    Mundanças cores e ajustes

--- loja_app/models.py
+++ loja_app/models.py
@@ -33,7 +33,7 @@ class Produto(models.Model):
     """
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
-    preco = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
     imagem = models.ImageField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
